09964e65488f17a4170771667e63ea7bc7351b9d
Łukasz Sałajczyk <lukasz.salajczyk@jazzy.pro>
2016-12-01 14:07:05 +0100

    Unit tests for Task.js

--- test/modules/Task.spec.js
+++ test/modules/Task.spec.js
@@ -1,9 +1,27 @@
 import Task from '../../src/models/Task';
 
 describe('Task', function() {
-  it('test', function(){
-    console.log(Task);
+  it('each created tasks should have different id', function(){
+    let task1 = Task.create('foo');
+    let task2 = Task.create('bar');
 
-    expect(2 + 2).toBe(5);
+    expect(task1.id).not.toBe(task2.id);
+  });
+
+  it('should filled property after created', function(){
+    let task = Task.create('foo');
+
+    expect(task.id).not.toBe(null);
+    expect(task.id).not.toBe(undefined);
+    expect(task.text).toBe('foo');
+    expect(task.created instanceof Date).toBe(true);
+    expect(task.isDone).toBe(false);
+  });
+
+  it('should have correct created date', function(){
+    let task = Task.create('foo');
+    let date = new Date();
+
+    expect(+task.created).toBe(+date);
   });
 });
